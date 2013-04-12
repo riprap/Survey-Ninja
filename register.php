03d977e4908393fcec493e49dce748d122ef1f2c
ScottMontgomery <scott@scottmontgomery.ca>
2013-04-12 11:46:50 -0400

    modified headers

---
+++ register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+    File Name: index.php
+    Authors Name: Scott Montgomery and Nolan Knill
+    Web Site Name: Survey Site
+    File Description: The index / home page of the sitee which displays an introduction and links to other 
+    sections of the site. 
+-->
+
+<?php 
+$page_name = "Home";
+include "functions/functions.php";
+include 'partials/html_header.php'; 
+?>
+  <body id="<?php echo strtolower($page_name);?>">
+
+  <?php include 'partials/header.php'; ?>
+  <?php include 'partials/footer.php'; ?>
+  
+  </body>
+</html>
