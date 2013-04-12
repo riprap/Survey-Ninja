a10c7f99d8eddee2dc83ed7efce6708596aede93
Nolan Knill <nolanknill@rogers.com>
2013-04-12 13:51:45 -0400

    Added pdo to db file, and added login template

--- db/database.php
+++ db/database.php
@@ -14,6 +14,31 @@ define('DB_PASSWORD', '');
 @mysql_connect('localhost', 'root', '')
   or die('Unable to connect to database');
 
-@mysql_select_db('portfolio')
-  or die('Unable to select portfolio');
+@mysql_select_db('survey')
+  or die('Unable to select database Survey');
 ;
+
+
+<?php
+/*
+    File Name: database.php
+    Authors Name: Scott Montgomery and Nolan Knill
+    Web Site Name: Survey Site
+    File Description: This file connects to the database.
+*/
+/*
+//enter database stuff here
+$host = "localhost";
+$database = "survey";
+$dsn = 'mysql:host=' . $host . ';dbname=' . $database . ';charset=UTF-8';
+$username = "sn";
+$password = "password";
+
+try{
+$db = new PDO($dsn, $username, $password);
+}
+catch (Exception $e){
+	exit($e);
+}
+*/
+?>
