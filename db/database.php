77a9b56819e21d843fd0d011878e97bcfc4240d5
Nolan Knill <nolanknill@rogers.com>
2013-04-17 11:36:55 -0400

    Survey functions implements PDO

--- db/database.php
+++ db/database.php
@@ -1,6 +1,6 @@
 <?php
 /*
-    File Name: database.php
+    File Name: db/database.php
     Authors Name: Scott Montgomery and Nolan Knill
     Web Site Name: Survey Site
     File Description: This file connects to the database, 
@@ -15,6 +15,9 @@ $password = "";
 
 try{
 	$db = new PDO($dsn, $username, $password);
+	//set error mode to most similar to mysql errors
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 }
 catch (Exception $e){
 	exit($e);
