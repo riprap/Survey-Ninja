6fa298120e0c6a94e2da2ba05457173caffbe00d
ScottMontgomery <scott@scottmontgomery.ca>
2013-04-18 00:36:31 -0400

    Changed spacing in database.php

--- db/database.php
+++ db/database.php
@@ -3,8 +3,7 @@
     File Name: db/database.php
     Authors Name: Scott Montgomery and Nolan Knill
     Web Site Name: Survey Site
-    File Description: This file connects to the database, 
-                      creating a PDO.
+    File Description: This file connects to the database, creating a PDO.                    
 */
 
 $host = "localhost";
@@ -14,12 +13,12 @@ $username = "root";
 $password = "";
 
 try{
-	$db = new PDO($dsn, $username, $password);
-	//set error mode to most similar to mysql errors
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  $db = new PDO($dsn, $username, $password);
+  //set error mode to most similar to mysql errors
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 }
 catch (Exception $e){
-	exit($e);
+  exit($e);
 }
 ?>
