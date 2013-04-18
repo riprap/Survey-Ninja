44a0df7102da56bb48badfc2a50f5ec8b44c00fe
ScottMontgomery <scott@scottmontgomery.ca>
2013-04-17 22:34:15 -0400

    Created new partial

---
+++ partials/get_login.php
@@ -0,0 +1,15 @@
+<?php 
+/*
+    File Name: get_login.php
+    Authors Name: Scott Montgomery and Nolan Knill
+    Web Site Name: Survey Site
+    File Description: This partial checks if the session id is set and performs the necessary actions
+*/
+
+ if (empty($_SESSION['id'])){
+  set_message("error", "You must be logged in to access this page.");
+  header('Location: login.php');
+  die;
+}
+
+$logged_in_profile = get_user($_SESSION['id']);   
