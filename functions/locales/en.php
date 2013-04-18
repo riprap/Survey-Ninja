eb6a434f559cf5b9ea5ec6f563607b2052a2a696
Nolan Knill <nolanknill@rogers.com>
2013-04-18 18:04:30 -0400

    Changed name of site from Survey Site to Survey Ninja

--- functions/locales/en.php
+++ functions/locales/en.php
@@ -7,7 +7,7 @@
 */
 
 
-$site_name = "Survey Site";
+$site_name = "Survey Ninja";
 $footer_text = "© Copyright SM and NK 2013";
 //$site_title = '<h1 class="title"><img class="logo" src="images/logo.png" alt="logo" width="50" height="50" />'.$site_name.'</h1>';
 $site_title = "<h1 class='title'>". $site_name ."</h1>";
