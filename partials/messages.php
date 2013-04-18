6d662a4164e03265163dfb831dd5e1def41fd887
ScottMontgomery <scott@scottmontgomery.ca>
2013-04-18 02:33:29 -0400

    updated control structure

--- partials/messages.php
+++ partials/messages.php
@@ -7,36 +7,30 @@
 */  
 $messages = get_messages();    
 ?>
-
-
-
    <div id="errors">
     <?php
       //Loop over each error in the errors array and print any errors that exist. 
-      if (isset($messages['error'])) {
-        foreach ($messages['error'] as $error) {
+      if (isset($messages['error'])) :
+        foreach ($messages['error'] as $error) :
           echo "<p>$error</p>";
-        }
-      }
+        endforeach;
+      endif;
       //Loop through any errors in form errors array
-      if (isset($errors)) {
-        foreach ($errors as $error) {
+      if (isset($errors)) :
+        foreach ($errors as $error) :
           echo "<p>$error</p>";
-        }
-      }
+        endforeach;
+      endif;
     ?>
   </div>
    <div id="success">
     <?php
       //Loop over each success in the success array and print any success that exist. 
-      if (isset($messages['success'])) {
-        foreach ($messages['success'] as $success) {
+      if (isset($messages['success'])) :
+        foreach ($messages['success'] as $success) :
           echo "<p>$success</p>";
-        }      
-      }
-      
-
-
+        endforeach;      
+      endif;   
     ?>
   </div>
 
