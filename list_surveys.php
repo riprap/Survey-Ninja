5e08c545753e30682a77fb611f84ffb92c5435bb
Nolan Knill <nolanknill@rogers.com>
2013-04-18 01:56:01 -0400

    List surveys style complete

--- list_surveys.php
+++ list_surveys.php
@@ -12,49 +12,54 @@ $surveys = get_active_surveys();
 ?>
 
 <?php include 'partials/html_header.php'; ?>
-  <body id="<?php echo strtolower($page_name);?>">
+<body id="<?php echo strtolower($page_name);?>">
 
-  <?php include 'partials/header.php'; ?>
-  <?php include 'partials/messages.php'; ?>
+	<?php include 'partials/header.php'; ?>
+    <div class="row">
+		<div class="large-9 columns" role="content">
+  		
+			<h3><?php echo $page_name;?></h3>
+  			<?php include 'partials/messages.php'; ?>
+			
+			<table>
+				<tr>
+					<th>Name</th>
+					<th>Survey Type</th>
+					<th>Number of Questions</th>
+				</tr>
+				<?php 
+				foreach ($surveys as $survey): 
+    				$questions = get_questions($survey['id']);
+  				?>
+    				<tr>
+      					<td>      
+        					<a href="survey.php?survey=<?php echo $survey['id']; ?>">
+          						<?php echo htmlentities($survey['name']); ?>
+	   						</a>
+						</td>
+						<td>
+							<?php echo $survey['survey_type'];?>
+						</td>
+						<td>
+						<?php 
+						if (!empty($questions)) :
+							echo count($questions);
+						else: ?>                
+							<a href="add_questions.php?survey=<?php echo $survey['id']; ?>">
+								<?php echo count($questions); ?>
+							</a>          
+						<?php 
+        				endif; //End the if statement to provide the url to add questions if the survey has no questions         
+        				?>
+      					</td>                         
+					</tr>
+				<?php endforeach; //End of the foreach to loop through each of the surveys ?>
 
-  <h1><?php echo $page_name;?></h1>
+			</table>
+		</div>
+	</div>
+	
+	<?php include 'partials/footer.php'; ?>
 
-  <table>
-    <tr>
-      <th>Name</th>
-      <th>Survey Type</th>
-      <th>Number of Questions</th>
-    </tr>
-  <?php foreach ($surveys as $survey): 
-    $questions = get_questions($survey['id']);
-  ?>
-    <tr>
-      <td>      
-        <a href="survey.php?survey=<?php echo $survey['id']; ?>">
-          <?php echo htmlentities($survey['name']); ?>
-        </a>
-      </td>
-      <td>
-        <?php echo $survey['survey_type'];?>
-      </td>
-      <td>
-        <?php 
-        if (!empty($questions)) :
-          echo count($questions);
-        else: ?>                
-          <a href="add_questions.php?survey=<?php echo $survey['id']; ?>">
-            <?php echo count($questions); ?>
-          </a>          
-        <?php 
-        endif; //End the if statement to provide the url to add questions if the survey has no questions         
-        ?>
-      </td>                         
-    </tr>
-  <?php endforeach; //End of the foreach to loop through each of the surveys ?>
-
-  </table>
-
-  <?php include 'partials/footer.php'; ?>
-  
-  </body>
+	</body>
 </html>
